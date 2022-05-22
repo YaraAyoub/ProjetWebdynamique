86ce52f0af235a0e4f6b5f26e6e1ab7c0e2283a2
[juthvt] <[julie3thevenot@hotmail.fr]>
2022-05-22 21:48:49 +0200

    modif parcours et index.css

--- parcours.js
+++ parcours.js
@@ -1,16 +1,28 @@
 function myFunction1() {
-var x = document.getElementById("myLinks1");
-if (x.style.display === "block") {
-x.style.display = "none";
-} else {
-x.style.display = "block";
+  var x = document.getElementById("myLinks1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+
 }
 
 function myFunction2() {
-var x = document.getElementById("myLinks2");
-if (x.style.display === "block") {
-x.style.display = "none";
-} else {
-x.style.display = "block";
+  var x = document.getElementById("myLinks2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
+
+function myFunction3() {
+    var x = document.getElementById("myLinks3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  
 }
