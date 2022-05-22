e15a753d2853d2298ac8acc8505bdf322159312d
[juthvt] <[julie3thevenot@hotmail.fr]>
2022-05-22 21:13:38 +0200

    add menu scroll

---
+++ parcours.js
@@ -0,0 +1,16 @@
+function myFunction1() {
+var x = document.getElementById("myLinks1");
+if (x.style.display === "block") {
+x.style.display = "none";
+} else {
+x.style.display = "block";
+}
+
+function myFunction2() {
+var x = document.getElementById("myLinks2");
+if (x.style.display === "block") {
+x.style.display = "none";
+} else {
+x.style.display = "block";
+}
+}
