cd3ac17a9859fe4b5e4583eb9491a3e897078d2f
[juthvt] <[julie3thevenot@hotmail.fr]>
2022-05-25 10:46:04 +0200

    html en php

--- parcourir.php
+++ parcourir.php
@@ -24,9 +24,9 @@
         <div class="btn-group">
           <a onclick="openFormCo()"><button class="button">Votre compte</button></a>
           <a href="rdv.php"><button class="button">RDV</button></a>
-          <a href="recherche.html"><button class="button">Recherche</button></a>
+          <a href="recherche.php"><button class="button">Recherche</button></a>
           <a href="parcourir.php"><button class="button">Tout Parcourir</button></a>
-          <a href="index.html"><button class="button">Accueil</button></a>
+          <a href="index.php"><button class="button">Accueil</button></a>
 
         </div>
       </div>
